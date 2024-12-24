9475c0944865be4d22bd512e127618d32847e5d2
rohankatira <rohankatira1@gmail.com>
2024-12-24 17:54:23 +0530

    done

--- vactor/question-1.cpp
+++ vactor/question-1.cpp
@@ -6,16 +6,28 @@ int main(){
 
     vector<int> v1={2,6,3,0,7,1,5};
         int n=v1.size(); //7
+
+        vector<int> vr(n);
+
         cout << "vector is :- ";
     for(int i=0;i<=n-1 ; i++){
         cout << v1[i] <<" ";
     }
         cout << endl;
 
-        cout << "Reversed vector is :- ";
-        for(int i=n-1; i>=0 ; i--){
-            cout << v1[i] <<" ";
+        
+        for(int i= 0; i< v1.size() ; i++)
+         {
+            n--;
+           vr[i]=v1[n];
         }
-            cout << endl;
+        cout << endl;
+         
+        cout << "Reversed vector is :- ";
+    for(int i= 0; i < vr.size(); i++){
+        cout << vr[i] <<" ";
+    }
+        cout << endl;
+          
     return 0;
-}
+}   
