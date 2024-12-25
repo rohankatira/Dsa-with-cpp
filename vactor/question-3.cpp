98c3959e5700dad7db137bdffec9759124a6c6c8
rohankatira <rohankatira1@gmail.com>
2024-12-25 19:02:02 +0530

    done

--- vactor/question-3.cpp
+++ vactor/question-3.cpp
@@ -24,7 +24,10 @@ int main()
     }
 
     cout << "smallest elements are : ";
-
+    for(int i=0; i<ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
     cout << endl;
 
     return 0;
